7fa1844b4071ebcfc31ab9ac3cdc9d3bdbba684b
Angeloamenta <Angeloamenta7@gmail.com>
2025-07-04 14:32:03 +0200

    add style

--- src/App.jsx
+++ src/App.jsx
@@ -20,10 +20,12 @@ function App() {
       <div>
       <h1 className='font-bold text-2xl mt-5 mb-5 text-center'>Formazione react</h1>
       </div>
+      <div className='container-personal'>
       <Routes>
         <Route path='/' element={<Home/>}/>
         <Route path='/esercizio-1' element={<Esercizio1/>}/>
       </Routes>
+      </div>
     </div>
     </BrowserRouter>
       
